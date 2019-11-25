2981d83c96d8ee67620700e3249fb5c35fe1b746
josuecarrillo505 <56973979+josuecarrillo505@users.noreply.github.com>
2019-11-25 07:47:38 -0500

    Create objeto.h

---
+++ objeto.h
@@ -0,0 +1,17 @@
+#ifndef UNTITLED11_OBJETO_H
+#define UNTITLED11_OBJETO_H
+
+
+class objeto {
+private:
+  palabra nombre;
+  int posicion*;
+  palabra color;
+public:
+  objeto(){};
+  void moverse();
+  void ubicarse_en_arena();
+};
+
+
+#endif //UNTITLED11_OBJETO_H
